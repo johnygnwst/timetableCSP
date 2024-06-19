0bb89f335eacfb0a9c7e54a4da41f0e9fcd0aed8
johnygnwst <92718963+johnygnwst@users.noreply.github.com>
2024-06-20 01:19:46 +0300

    Update mrv.py

--- mrv.py
+++ mrv.py
@@ -1,12 +1,12 @@
+from random import shuffle
 
 from csp_init import *
-
 
 mrv_domains = {}
 counter = 0
 
 
-#init empty assignment
+# init empty assignment
 def init_assignment_mrv(csp):
   global mrv_domains
   assignment = {}
@@ -15,24 +15,43 @@ def init_assignment_mrv(csp):
     mrv_domains[var] = csp[DOMAINS].copy()
   return assignment
 
-#mrv backtracking
+
+def getRoom(csp, assignment, var, value):
+  rooms = data._rooms
+  rooms.sort(key=lambda c: c[1])
+
+  for r in rooms:
+    if (r[1] >= var._number_of_students):
+      free = True
+      for k in csp[VARIABLES]:
+        if (assignment[k] is not None):
+          if (k._room == r and assignment[k] == value):
+            free = False
+      if free:
+        return r
+
+
+# mrv backtracking
 def mrv_backtracking(assignment, csp):
   global counter
   global mrv_domains
+  rnd_domains_m = csp[DOMAINS][:]
   if is_complete(assignment):
     return assignment
-  var = find_mrv(assignment,csp)
-  for value in csp[DOMAINS]:
+  var = find_mrv(assignment, csp)
+  shuffle(rnd_domains_m)
+  for value in rnd_domains_m:
     if (is_in_domain(value, mrv_domains[var])):
       assignment[var] = value
-      counter+=1
-      add_to_mrv_domains(assignment,csp,var)
+      counter += 1
+      add_to_mrv_domains(assignment, csp, var)
+      var._room = getRoom(csp, assignment, var, value)
       if is_consistent(assignment, csp[CONSTRAINTS]):
         result = mrv_backtracking(assignment, csp)
         if result != FAILURE:
           return result
       assignment[var] = None
-      undo(assignment,csp)
+      undo(assignment, csp)
   return FAILURE
 
 
@@ -43,13 +62,11 @@ def undo(assignment, csp):
   for i in csp[VARIABLES]:
     for j in csp[VARIABLES]:
       if (assignment[j] is not None):
-        if (not i==j):
+        if (not i == j):
           if (i._teacher == j._teacher):
             for k in range(len(mrv_domains[i])):
               if (mrv_domains[i][k] == assignment[j]):
                 mrv_domains[i][k] = None
-         
-
 
 def is_in_domain(value, domain):
   for d in domain:
@@ -63,12 +80,12 @@ def add_to_mrv_domains(assignment, csp, value):
   global prev_assignment
   j = value
   for i in csp[VARIABLES]:
-    if (not i==j):
+    if (not i == j):
       if (i._teacher == j._teacher):
         for k in range(len(mrv_domains[i])):
           if (mrv_domains[i][k] == assignment[j]):
             mrv_domains[i][k] = None
-    
+
 
 def get_domains():
   global mrv_domains
@@ -79,7 +96,7 @@ def domain_len(domain):
   res = 0
   for d in domain:
     if d is not None:
-      res +=1
+      res += 1
   return res
 
 
