5762d1fa95f286148950c74ed1e93e9d72c5d190
johnygnwst <92718963+johnygnwst@users.noreply.github.com>
2024-06-20 01:15:34 +0300

    Update data.py

--- data.py
+++ data.py
@@ -7,21 +7,18 @@ class Data:
     def __init__(self):
         self._days = DAYS
         self._meetingTimes = MEETING_TIMES
-        self._subjects = self.init_subjects(SUBJECTS)
+        self._subjects = self.init_subjects(SUBJECTS, SUBJ_TEACH)
         self._specs = self.init_specs(SPECIALITIES)
         self._rooms = ROOMS
         self._classes = self.init_classes()
-      
-    
-        
 
-    def init_subjects(self, subjects):
+    def init_subjects(self, subjects, subj_teach):
         subj = []
         for s in subjects:
             name = s["name"]
             num = s["number_of_students"]
             gr = s["groups"]
-            tea = s["teacher"]
+            tea = rnd.choice(subj_teach[s["name"]])
             subj.append(Subject(name, num,gr, tea))
         return subj
 
@@ -49,7 +46,8 @@ class Data:
         classes = []
         for sp in self._specs:
             for sub in sp._subjects:
-                lect = Class(sp, sub, None, None, None, "lecture")
+                st1 = "Lecture"
+                lect = Class(sp, sub, None, None, None, st1)
                 classes.append(lect)
                 i = 1
                 for g in range(sub._groups):
